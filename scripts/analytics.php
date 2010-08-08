d4ed878169ef3186891df74a2fd9f9d45d647e2d
fredericgalline <fredericgalline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-08 07:33:14 +0000

    mise à jour analytics
    git-svn-id: https://plugins.svn.wordpress.org/easy-toolbox/trunk@273055 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scripts/analytics.php
+++ scripts/analytics.php
@@ -1,28 +1,25 @@
 <?php
-
-function analytics() {
-	
+function analytics() {	
 	$adsense = get_option('etb_analytics');
 	$name = get_bloginfo('name');
-	if (isset($adsense) && !empty($adsense)) {
-	?>	
+?>	
 	
 <!-- Analytics client -->
 <script type="text/javascript">
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', '<?php echo $adsense ?>' ],['_trackPageview']);_gaq.push(['_setAccount', 'UA-116659-18'],['_setDomainName', 'none'],['_setAllowLinker', true],['_setAllowHash', false]);
+<?php if (isset($adsense) && !empty($adsense)) { ?>
+_gaq.push(['_setAccount', '<?php echo $adsense ?>' ],['_trackPageview']);
+<?php } ?>
+_gaq.push(['ETB._setAccount', 'UA-116659-18'],['ETB._setDomainName', 'none'],['ETB._setAllowLinker', true],['ETB._setAllowHash', false]);
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 </script>
-
-
+	
 <?php
-
 }
-
 add_action('wp_head', 'analytics');
 
 ?>
