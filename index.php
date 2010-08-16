f49cee6e277de9a12ed1aa380bcee65425c1187a
fredericgalline <fredericgalline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-16 11:32:32 +0000

    0.4.1 bug content.php with url
    git-svn-id: https://plugins.svn.wordpress.org/easy-toolbox/trunk@276435 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy Toolbox
 Plugin URI: http://easytoolbox.net
 Description: All In One plugin for SEO, Facebook, Twitter Flickr, Adsense, Analytics and more ....
-Version: 0.4
+Version: 0.4.1
 Author: Frederic Galline;
 Author URI: http://galline.fr
 License: A "Slug" license name e.g. GPL2
@@ -31,7 +31,7 @@ load_plugin_textdomain( 'easytoolbox', false, basename(dirname(__FILE__)) . '/la
 
 
 // Version du plugin
-$_SESSION['version'] = "0.4";
+$_SESSION['version'] = "0.4.1";
 
 // ajouter le menu en admin
 function admin_menu() {
