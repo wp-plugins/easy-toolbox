5c3d40532859e3bd5e37d4e7a8675b08f39503f8
fredericgalline <fredericgalline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-25 10:41:47 +0000

    0.5 Facebook like bouton options
    git-svn-id: https://plugins.svn.wordpress.org/easy-toolbox/trunk@280521 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scripts/facebook.php
+++ scripts/facebook.php
@@ -1,31 +1,59 @@
 <?php
 
 function facebook() {
+	
+// admettre les thumbnails //
+/* add_theme_support('post-thumbnails');
+add_image_size( 'facebook', 50, 50, true ); */
 
-if (is_single()) {
-global $post, $posts;
+$url=get_permalink();
+$facebook_id = get_option('etb_facebookid');
 
-//get_the_post_thumbnail( $post->ID,'facebook') );
-
-//$image_id = get_post_thumbnail_id( $post->ID, 'facebook' );  
-//$image_url = wp_get_attachment_image_src($image_id,'facebook');  
-//$image_url = $image_url[0]; 
-$facebook = get_option('etb_facebook');
-$facebookid = get_option('etb_facebookid');
-
-	if (isset($facebookid) && !empty($facebookid)) {
-		echo '<meta property="fb:admins" content="'.$facebookid.'"/>'."\n";
+	if (is_single() or is_page()) {
+	global $post, $posts;
+	$image_id = get_post_thumbnail_id( $post->ID, 'facebook' );  
+	$image_url = wp_get_attachment_image_src($image_id,'facebook');  
+	$image_url = $image_url[0]; 
+	
+	$facebook = get_option('etb_facebook');
+	$id_apps_facebook = get_option('etb_idapps_facebook');
+	
+	echo "\n".'<!-- Config facebook -->'."\n";	
+	echo '<meta property="og:site_name" content="'.get_bloginfo('name').'"/>'."\n";
+	echo '<meta property="og:title" content="'.get_the_title().'"/>'."\n";
+	echo '<meta property="og:description" content="'.get_description($post).' ..."/>'."\n";
+	echo '<meta property="og:image" content="'.$image_url.'"/>'."\n";
+	echo '<meta property="og:type" content="blog" />'."\n";
+	echo '<meta property="fb:app_id" content="'.get_option('etb_idapps_facebook').'"/>'."\n";
+	echo '<meta property="og:url" content="'.$url.'"/>'."\n";
+		if (isset($facebook_id) && !empty($facebook_id)) {
+			echo '<meta property="fb:admins" content="'.$facebook_id.'"/>'."\n";
+		}	
 	}
-echo '<meta property="og:site_name" content="'.get_bloginfo('name').'"/>'."\n";
-echo '<meta property="og:title" content="'.get_the_title().'"/>'."\n";
-//echo '<meta property="og:description" content="'.get_description($post).' ..."/>'."\n";
-//echo '<meta property="og:image" content="'.$image_url.'"/>'."\n";
-
-}
 		
 }
 
 add_action('wp_head', 'facebook');
 
+function facebook_script() {
+	?>
+	
+<div id="fb-root"></div>
+<script>
+ window.fbAsyncInit = function() {
+ FB.init({appId: '<?php echo get_option('etb_idapps_facebook') ?>', status: true, cookie: true,
+  xfbml: true});
+ };
+ (function() {
+ var e = document.createElement('script'); e.async = true;
+ e.src = document.location.protocol +
+  '//connect.facebook.net/<?php bloginfo('language') ?>/all.js';
+ document.getElementById('fb-root').appendChild(e);
+ }());
+</script>
+	<?
+	}
+
+add_action('wp_footer', 'facebook_script');
 
 ?>
