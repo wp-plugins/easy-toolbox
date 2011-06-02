bbb54d3f8c1374b0da8782e9508eaae4ba476920
fredericgalline <fredericgalline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-02 18:41:57 +0000

    version 1
    git-svn-id: https://plugins.svn.wordpress.org/easy-toolbox/trunk@392455 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ option.php
@@ -0,0 +1,70 @@
+<div class="wrap">
+	<div id="poststuff">
+		<div id="post-body">
+		
+
+	<h2><?php _e('Options', 'easytoolbox') ?></h2>
+		
+<!-- tableau Admin Social -->
+<form method="post" action="options.php">
+<?php settings_fields('easytoolbox_options'); ?>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$("a.gallery").fancybox({
+		'transitionIn'	:'elastic',
+        'transitionOut'	:'elastic', 
+        'easingIn'		:'easeOutBack',
+		'easingOut'		:'easeInBack', 
+        'width'			:750, 
+        'speedIn'		:600,
+        'speedOut'		:250,  
+        });
+	$("a#single_image").fancybox();
+
+	
+	$("a#inline").fancybox({
+		'hideOnContentClick': true,
+		'transitionIn'	:'elastic',
+        'transitionOut'	:'elastic', 
+        'easingIn'		:'easeOutBack',
+		'easingOut'		:'easeInBack', 
+        'speedIn'		:600,
+        'speedOut'		:200, 
+        'autoDimensions':true,
+	});
+
+	$("a#iframe").fancybox({
+		'transitionIn'	:'elastic',
+		'transitionOut'	:'elastic',
+		'speedIn'		:600, 
+		'speedOut'		:200, 
+		'width' 		:'85%',
+		'height' 		:'85%', 
+		'overlayShow'	:false,
+		'type'          :'iframe', 
+	});
+
+});
+
+$(document).ready(function() {
+
+});
+
+</script>	
+	
+	<?php 
+	include 'HTML/optimiz.php'; 
+	include 'HTML/socialize.php'; 
+	include 'HTML/monetize.php'; 
+	?>
+	
+	<p class="submit" >
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+	</p>
+	
+</form>
+	
+		</div><!--postbody-->
+	</div><!--poststuff-->
+</div> <!-- wrap -->
