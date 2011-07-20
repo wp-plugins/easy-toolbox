5ef3fd260920af675b8de9a0aca52f4468c407b5
fredericgalline <fredericgalline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-20 09:14:51 +0000

    header index.php
    git-svn-id: https://plugins.svn.wordpress.org/easy-toolbox/trunk@412422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,34 +3,19 @@
 Plugin Name: Easy Toolbox
 Plugin URI: http://easytoolbox.net
 Description: All In One plugin for SEO, Facebook, Twitter Flickr, Adsense, Google Analytics, GetClicky and more ....
-Version: 1.3
+Version: 1.31
 Author: Frederic Galline;
 Author URI: http://galline.fr
 License: A "Slug" license name e.g. GPL2
 Domain Path: /languages
 */
 
-/*  Copyright 2010  Galliné Frédéric  (email : plugin@easytoolbox.net)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 session_start();
 
 load_plugin_textdomain( 'easytoolbox', false, basename(dirname(__FILE__)) . '/languages/' );
 
 // Version du plugin
-$_SESSION['version'] = "1.3";
+$_SESSION['version'] = "1.31";
 
 // ajouter le menu en admin
 function admin_menu() {
